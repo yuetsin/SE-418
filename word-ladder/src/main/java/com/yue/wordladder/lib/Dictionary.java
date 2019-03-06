3bb89207d18969c645a5666b0658d2be255e0779
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-03-06 18:52:05 +0800

    分出 Dictionary 模块。

---
+++ word-ladder/src/main/java/com/yue/wordladder/lib/Dictionary.java
@@ -0,0 +1,10 @@
+package com.yue.wordladder.lib;
+
+public class Dictionary {
+
+    private static String[] dictionary = {"cat", "dog"};
+
+    public static String[] getDictionary() {
+        return dictionary;
+    }
+}
