cdda4a46343aaf9a115de62b180d851b3b183fc2
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-03-07 13:51:04 +0800

    轻改 Controller。

--- word-ladder/src/main/java/com/yue/wordladder/controller/Controller.java
+++ word-ladder/src/main/java/com/yue/wordladder/controller/Controller.java
@@ -8,17 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @RequestMapping("/")
     public String IndexPage() {
-        return "<h1>Index Page</h1> <h2>Tips</h2> Visit <b> /ladder?start=start_word&end=end_word </b> to use word ladder program.";
+        return "<h1>Here's my Word Ladder Project.</h1> <h2>Tips</h2> Visit <b> /ladder?start=start_word&end=end_word </b> to use word ladder program.";
     }
 
     @RequestMapping("/ladder")
     public String FeaturePage(String start, String end) {
         if (start == null || end == null) {
-            return "Invalid Argument. Visit <b> /ladder?start=start_word&end=end_word </b> to use word ladder program.";
+            return "<h2>Invalid Argument.</h2> Visit <b> /ladder?start=start_word&end=end_word </b> to use word ladder program.";
         }
         LadderHelper lh = new LadderHelper();
         Dictionary dict = lh.generateDict();
         String[] rst = lh.calculateLadder(start, end, dict);
-        return String.format("You requested a ladder from <b>%s</b> to <b>%s</b>.", start, end);
+        return String.format("<h2>Got it.</h2>You requested a ladder from <b>%s</b> to <b>%s</b>.", start, end);
     }
 }
