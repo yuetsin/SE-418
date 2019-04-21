0f9ce08e4ae14895726e5b075006f65a55e88c13
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-04-21 19:00:23 +0800

    Add role control.

--- word-ladder/src/main/java/com/yue/wordladder/controller/ConsoleController.java
+++ word-ladder/src/main/java/com/yue/wordladder/controller/ConsoleController.java
@@ -1,10 +1,12 @@
 package com.yue.wordladder.controller;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class ConsoleController {
     @RequestMapping("/console")
     @PreAuthorize("hasRole('ADMIN')")
