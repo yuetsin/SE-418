b107ddaaf5cc0c2a6c51ef5c12cd4096b9927e27
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-05-06 10:42:51 +0800

    完成一个拆分。

--- word-ladder/src/main/java/com/yue/wordladder/controller/JSONController.java
+++ word-ladder/src/main/java/com/yue/wordladder/controller/JSONController.java
@@ -6,39 +6,18 @@ import com.yue.wordladder.ladder.LadderHelper;
 import com.yue.wordladder.response.Response;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.List;
 
-
-
 @RestController
 @RequestMapping("/json")
 public class JSONController {
     @RequestMapping("/ladder")
-    public Response GetJSON(String start, String end) {
-        System.out.println(String.format("Get a JSON Request from %s to %s", start, end));
-        Response resp = new Response();
-        if (start == null || end == null) {
-            resp.status = "bad parameters";
-            return resp;
-        }
-        LadderHelper lh = new LadderHelper();
-        Dictionary dict = lh.generateDict();
-        List<String> rst;
-        try {
-            rst = lh.calculateLadder(start, end, dict).get(0);
-        } catch (Exception e) {
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            e.printStackTrace(pw);
-            resp.status = "no possible ladder";
-            return resp;
-        }
-
-        resp.status = "ok";
-        resp.ladders = rst.toArray(new String[0]);
-        return resp;
+    public ModelAndView GetJSON(String start, String end) {
+        return new ModelAndView(new RedirectView(String.format("http://47.103.66.98:8080/practical/json/ladder?start=%s&end=%s", start, end)));
     }
 }
