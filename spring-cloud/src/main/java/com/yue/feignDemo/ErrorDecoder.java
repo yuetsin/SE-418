b1dcc319b70009ffdff24e64e97af862c9d872ea
yuetsin <akaza_akari@sjtu.edu.cn>
2019-05-27 21:52:40 +0800

    [change] from .properties to configuration class

--- spring-cloud/src/main/java/com/yue/feignDemo/ErrorDecoder.java
+++
@@ -1,4 +0,0 @@
-package com.yue.feignDemo;
-
-public class ErrorDecoder {
-}
--- spring-cloud/src/main/java/com/yue/feignDemo/UserFeignClient.java
+++ spring-cloud/src/main/java/com/yue/feignDemo/UserFeignClient.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "microservice-provider-user", fallback = UserFeignClientFallback.class)
+@FeignClient(name = "microservice-provider-user", configuration = FeignConfiguration.class, fallback = UserFeignClientFallback.class)
 public interface UserFeignClient {
-    @GetMapping("/counter")
-    String getCount();
+    @GetMapping("/counter/{number}/{plus}")
+    String sumUp();
 }
